5105abf43dd3f321dfe97d4edf45bbfe6664870b
Kay Kim <kimjh@bawi.org>
2020-08-29 17:31:02 +0900

    add squared logo, remove two-lined logo

--- components/Logo/SquaredLogo.tsx
+++ components/Logo/SquaredLogo.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 const Root = styled.div`
   margin: 1rem 0;
-  width: 150px;
+  width: 40px;
   align-self: center;
   &:hover {
     cursor: pointer;
@@ -21,7 +21,7 @@ const OneLineLogo: React.FC<Props> = ({ ...props }) => {
   return (
     <Root {...props}>
       <Link href="/">
-        <img alt="onDisplay" src="/logo/logo-one-line.svg" />
+        <img alt="onDisplay" src="/logo/logo-squared.svg" />
       </Link>
     </Root>
   );
